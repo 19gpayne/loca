94acb3606bb422b9ebc26f67eb0f81842d6ebe0d
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2022-04-10 07:44:47 -0400

    Fixed some issues

--- api/route/home.py
+++ api/route/home.py
@@ -1,13 +1,20 @@
 import math
 from flask import Blueprint, request
-from gcloud import upload_images_from_monuments, upload_base64_image, create_product, get_similar_products_file
+from gcloud import (
+    upload_images_from_monuments,
+    upload_base64_image,
+    create_product,
+    get_similar_products_file,
+)
 
 from redis_init import redis_client
+import json
 import uuid
 
-home_api = Blueprint('api', __name__)
+home_api = Blueprint("api", __name__)
 
-@home_api.route('/')
+
+@home_api.route("/")
 def welcome():
     """
     1 liner about the route
@@ -16,36 +23,43 @@ def welcome():
     """
     return "Welcome to loca!", 200
 
-@home_api.route('/get_monument/', methods=['GET'])
+
+@home_api.route("/get_monument/", methods=["GET"])
 def get_monument():
     """
     Gets a monument with the given ID
     Request format: ?id="monument id"
     """
-    monument_id = request.args['id']
-    return redis_client.get(monument_id), 200
+    monument_id = request.args["id"]
+    return redis_client.getConnection().get(monument_id), 200
 
-@home_api.route('/add_monument', methods=['POST'])
+
+@home_api.route("/add_monument", methods=["POST"])
 def add_monument():
     """
     Adds a monument with the given description
     JSON format: { lat: <lat>, lon: <lon>, title: "title", description: "description", image: "b64 image" }
     """
-    req = request.get_json()
+    # req = request.get_json()
+    req = json.loads(request.data, strict=False)
 
     product_id = str(uuid.uuid4())
-    uri = upload_base64_image(req["image"])
-    create_product(product_id, req['title'])
+    uri = req["image"]
+    if not uri.startswith("gs"):
+        uri = upload_base64_image(uri)
+
+    create_product(product_id, req["title"])
 
     description = {
-        "lat" : req['lat'],
-        "lon" : req['lon'],
-        "title": req['title'],
-        "description": req['description'],
-        "picture": uri
+        "lat": req["lat"],
+        "lon": req["lon"],
+        "title": req["title"],
+        "description": req["description"],
+        "picture": uri,
     }
-    redis_client.set(str(uuid.uuid4()), description)
+    redis_client.getConnection().set(str(uuid.uuid4()), description)
     return True, 200
+
 
 # Credit to cs95 on Stack Overflow
 def get_distance(lat_1, lng_1, lat_2, lng_2):
@@ -54,15 +68,14 @@ def get_distance(lat_1, lng_1, lat_2, lng_2):
     d_lng = lng_2 - lng_1
 
     temp = (
-         math.sin(d_lat / 2) ** 2
-       + math.cos(lat_1)
-       * math.cos(lat_2)
-       * math.sin(d_lng / 2) ** 2
+        math.sin(d_lat / 2) ** 2
+        + math.cos(lat_1) * math.cos(lat_2) * math.sin(d_lng / 2) ** 2
     )
 
     return 6373.0 * (2 * math.atan2(math.sqrt(temp), math.sqrt(1 - temp)))
 
-@home_api.route('/find_monument', methods=['POST'])
+
+@home_api.route("/find_monument", methods=["POST"])
 def find_monument():
     """
     Find a monument given the picture, latitude, and longitude
@@ -70,17 +83,19 @@ def find_monument():
     JSON format: { image: <b64 image> }
     """
     req = request.get_json()
-    user_loc = (float(request.args['lat']), float(request.args['lon']))
+    user_loc = (float(request.args["lat"]), float(request.args["lon"]))
 
-    matches = get_similar_products_file(req['image'], None, 5)
+    matches = get_similar_products_file(req["image"], None, 5)
 
     best_monument = None
     best_monument_score = None
 
     for match in matches:
         product = match.product
-        monument = redis_client.get(product.name)
-        monument_dist = get_distance(user_loc[0], user_loc[1], float(monument['lat']) , float(monument['lon']))
+        monument = redis_client.getConnection().get(product.name)
+        monument_dist = get_distance(
+            user_loc[0], user_loc[1], float(monument["lat"]), float(monument["lon"])
+        )
 
         if monument_dist > 1:
             continue
@@ -92,16 +107,17 @@ def find_monument():
 
     return best_monument, 200
 
-@home_api.route('/edit_description', methods=['POST'])
+
+@home_api.route("/edit_description", methods=["POST"])
 def edit_description():
     """
     Edits the description of an object
     JSON request format {id: "id", desc: "desc", date: yyyy/mm/dd}
     """
     req = request.get_json()
-    description = redis_client.get(req['id'])
-    description['description'] = req['description']
-    description['datetime'] = req['datetime']
-    redis_client.set(req['id'], description)
+    description = redis_client.getConnection().get(req["id"])
+    description["description"] = req["description"]
+    description["datetime"] = req["datetime"]
+    redis_client.getConnection().set(req["id"], description)
 
     return True, 200
